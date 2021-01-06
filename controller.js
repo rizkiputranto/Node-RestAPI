6247381cdc33cbe5d4978aa7008601597bf060cc
rizki <rizki.putranto@ozip.co.id>
2021-01-07 00:04:04 +0700

    menampilkan data mahasiswa

--- controller.js
+++ controller.js
@@ -3,6 +3,17 @@
 var response = require('./res');
 var connection = require('./connection');
 
-exports.index = function (req, res){
+exports.index = function (req, res) {
     response.ok('Rest API status running', res);
 };
+
+//Mengambil Data Mahasiswa dari Database
+exports.tampilMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if (error) {
+            connection.log('error');
+        } else {
+            response.ok(rows, res);
+        }
+    });
+}
